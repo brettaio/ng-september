dfbbadd11ec388b03e49c2d0da4de27e71a1449f
Brett Connell <dev@bretta.io>
2023-08-31 09:00:14 -0400

    ready to push to CICD to see if create check out session shows expected return data into card component

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,34 +1,34 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
+  stripeSession: any;
+
   constructor(private http: HttpClient) {}
 
-  testButtonClick() {
-    const observer = {
-      next: (response: any) => {
-        console.log('Serverless function response:', response);
-      },
-      error: (error: any) => {
-        console.error('Error calling serverless function:', error);
-      },
-    };
-
+  initiatePayment() {
     this.http
-      .get('/.netlify/functions/testFunction')
+      .post('/functions/createCheckoutSession', {})
       .pipe(
-        tap((response) => observer.next(response)),
         catchError((error) => {
-          observer.error(error);
+          console.error('Error initiating payment:', error);
           throw error;
         })
       )
-      .subscribe();
+      .subscribe((response: any) => {
+        this.stripeSession = response;
+        console.log('Stripe session info:', this.stripeSession);
+
+        const sessionId = response.sessionId;
+
+        // Redirect the user to the Stripe checkout page
+        window.location.href = `https://checkout.stripe.com/pay/${sessionId}`;
+      });
   }
 }
