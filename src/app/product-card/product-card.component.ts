7e80e04e1cc2062748f4500c6f1b7c6a88d2eea2
Brett Connell <dev@bretta.io>
2023-08-31 09:21:06 -0400

    last one before off to daycare, I am attempting to have the card component call he serverless function, get the returned value and show it on the card componenet

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 
+declare var Stripe: any;
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
+  stripe: any;
   stripeSession: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.stripe = Stripe(
+      'pk_test_51NbRDjL0dOKck8e30IKHMmqdys7VDRdusIGlePpcyPgmPdYqqcum5MYFH4bTgnIGF41aXsZTldY56Lfrqphop2xH00fislaiOR'
+    ); // Replace with your Stripe public key
+  }
 
   initiatePayment() {
     this.http
@@ -22,13 +29,18 @@ export class ProductCardComponent {
         })
       )
       .subscribe((response: any) => {
-        this.stripeSession = response;
+        this.stripeSession = response.sessionId;
         console.log('Stripe session info:', this.stripeSession);
 
-        const sessionId = response.sessionId;
-
-        // Redirect the user to the Stripe checkout page
-        window.location.href = `https://checkout.stripe.com/pay/${sessionId}`;
+        this.stripe
+          .redirectToCheckout({
+            sessionId: this.stripeSession,
+          })
+          .then((result: any) => {
+            if (result.error) {
+              console.error('Error initiating payment:', result.error);
+            }
+          });
       });
   }
 }
