35f7ce9a1e8c465b2c74aca74e31b5602ce935d1
Brett Connell <dev@bretta.io>
2023-09-01 14:03:04 -0400

    kinda lost on the progress, will push and see what happens net side

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -1,29 +1,55 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { Feedback } from 'src/app/feedback/feedback';
+import { NetlifyFormsService } from 'src/app/netlify-forms/netlify-forms.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
-  onSubmit() {
-    throw new Error('Method not implemented.');
-  }
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private netlifyForms: NetlifyFormsService
+  ) {}
 
   feedbackForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', [Validators.email, Validators.required]],
-    type: ['', Validators.required],
-    description: ['', Validators.required],
-    rating: [0, Validators.min(1)],
+    firstName: ['', [Validators.required, Validators.nullValidator]],
+    lastName: ['', [Validators.required, Validators.nullValidator]],
+    email: [
+      '',
+      [Validators.email, Validators.required, Validators.nullValidator],
+    ],
+    type: ['', [Validators.required, Validators.nullValidator]],
+    description: ['', [Validators.required, Validators.nullValidator]],
+    rating: [0, [Validators.min(1), Validators.nullValidator]],
   });
 
   errorMsg = '';
 
   closeError() {
     this.errorMsg = '';
+  }
+  onSubmit() {
+    const formData: Feedback = {
+      firstName: this.feedbackForm.get('firstName')?.value || '',
+      lastName: this.feedbackForm.get('lastName')?.value || '',
+      email: this.feedbackForm.get('email')?.value || '',
+      type: this.feedbackForm.get('type')?.value || '',
+      description: this.feedbackForm.get('description')?.value || '',
+      rating: this.feedbackForm.get('rating')?.value || 0,
+    };
+
+    this.netlifyForms.submitFeedback(formData).subscribe(
+      () => {
+        this.feedbackForm.reset();
+        this.router.navigateByUrl('/success');
+      },
+      (err) => {
+        this.errorMsg = err;
+      }
+    );
   }
 }
