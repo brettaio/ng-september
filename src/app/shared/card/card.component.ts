4ffb0eb2418093681d3e2720f6f86353e62d2e07
Brett Connell <dev@bretta.io>
2023-09-10 13:37:42 -0400

    functions not function

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -17,7 +17,7 @@ export class CardComponent {
 
     // Create a new Checkout Session using the server-side endpoint
     // You can replace '/api/stripe' with your actual server-side endpoint
-    fetch('/function/createCheckoutSession', {
+    fetch('/functions/createCheckoutSession', {
       method: 'POST',
     })
       .then((response) => response.json())
