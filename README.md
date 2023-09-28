149fac872aa1d102a4dd31e02a347adeb92ee343
Brett Connell <dev@bretta.io>
2023-09-28 06:39:43 -0400

    updating

--- README.md
+++ README.md
@@ -22,6 +22,18 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
-## Further help
+## Ng September here we go, months going good, just under half way.
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7105602415831224320" height="410" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+
+<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7104833391484682240" height="748" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+
+<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7104151040282824705" height="863" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+
+- **Service Excellence:** You can find me on Fiverr, where I've had the pleasure of delivering high-quality services to clients. Feel free to explore my [Fiverr profile](#link-to-your-fiverr-profile) for more details.
+
+Let's connect on LinkedIn and continue this journey of growth and learning together:
+
+- [LinkedIn Profile](#link-to-your-linkedin-profile)
+
+This journey is all about setting goals, learning, growing, and enjoying every step of the way. Thanks for joining me!
--- functions/emailOpen.js
+++ functions/emailOpen.js
@@ -1,57 +1,29 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-exports.handler = async (event, context) => {
-  try {
-    // Parse various request headers
-    const ipAddress = event.headers['client-ip'] || event.requestContext.identity.sourceIp;
-    const userAgent = event.headers['user-agent'];
-    const referer = event.headers['referer'] || event.headers['referrer'];
-    const authorization = event.headers['authorization'];
-    const contentType = event.headers['content-type'];
-    const accept = event.headers['accept'];
-    const cookies = event.headers['cookie'];
-    const host = event.headers['host'];
-    const acceptLanguage = event.headers['accept-language'];
-    const cacheControl = event.headers['cache-control'];
+// Define the EmailOpen Schema
+const emailOpenSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  ipAddress: String,
+  userAgent: String,
+  referer: String,
+  authorization: String,
+  contentType: String,
+  accept: String,
+  cookies: String,
+  host: String,
+  acceptLanguage: String,
+  cacheControl: String,
+  // Add other fields as needed
+});
 
-    // Create an object to store the request data
-    const requestData = {
-      ipAddress,
-      userAgent,
-      referer,
-      authorization,
-      contentType,
-      accept,
-      cookies,
-      host,
-      acceptLanguage,
-      cacheControl,
-      // Add other headers as needed
-    };
+// Create the EmailOpen model
+const EmailOpen = mongoose.model('EmailOpen', emailOpenSchema);
 
-    // Connect to MongoDB
-    const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
-
-    // Insert request data into a MongoDB collection
-    const db = client.db('emailTrackTest');
-    const collection = db.collection('emailOpen');
-    await collection.insertOne(requestData);
-
-    // Close the MongoDB connection
-    client.close();
-
-    console.log('Request data inserted into MongoDB.');
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify('Request data logged'),
-    };
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify('Internal Server Error'),
-    };
-  }
-};
+module.exports = EmailOpen;
